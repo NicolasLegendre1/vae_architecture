8183b07af9a5012bce32f08fdd3002ee07b54775
NicolasLegendre1 <nicolaslegendre963@yahoo.com>
2021-08-05 21:11:16 +0200

    test

--- tests/test_pinchon_hoggan_dense.py
+++ tests/test_pinchon_hoggan_dense.py
@@ -1,7 +1,6 @@
 """Test pinchon_hoggan_dense"""
 
 import torch
-
 from wigner_representation import pinchon_hoggan_dense as phd
 
 
@@ -18,5 +17,16 @@ class TestPinchon:
         angle = 1
         la = 1
         matrix = phd.z_rot_mat(angle, la)
-        assert matrix.shape == 3
+        assert matrix.shape == (3, 3)
         assert type(matrix) == torch.Tensor
+
+    @staticmethod
+    def test_rot_mat():
+        alpha = 1
+        beta = 1
+        gamma = 1
+        la = 2
+        J = phd.open_Jd()[2]
+        matrix = phd.rot_mat(alpha, beta, gamma, la, J)
+        assert type(matrix) in torch.Tensor
+        assert matrix.shape == (5, 5)
