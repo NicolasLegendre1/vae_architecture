c1ef45ff1157b34c1c0c01e011113496dfac2f51
NicolasLegendre1 <nicolaslegendre963@yahoo.com>
2021-08-05 20:56:53 +0200

    test

--- tests/test_pinchon_hoggan_dense.py
+++ tests/test_pinchon_hoggan_dense.py
@@ -11,3 +11,11 @@ class TestPinchon:
 
         assert type(Jd[0]) is torch.Tensor
         assert len(Jd) == 150
+
+    @staticmethod
+    def test_z_rot_mat():
+        angle = 1
+        la = 1
+        matrix = phd.z_rot_mat(angle, la)
+        assert matrix.shape == 3
+        assert type(matrix) == torch.Tensor
